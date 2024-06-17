cc20c37ee8254dd667e5afc004c6914a73ce7ce5
devmatheusnunesdebarros@gmail.com <ti@apsh.local>
2024-06-17 17:18:30 -0300

    Construido filtro

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,19 @@
-const BarraLateral = () => <aside> Barra lateral </aside>
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar"></S.Campo>
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
